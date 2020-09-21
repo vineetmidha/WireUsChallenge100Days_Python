a5faed1386d18ea7d64f2784a55072ef878023fe
vineetmidha <50925587+vineetmidha@users.noreply.github.com>
2020-09-21 12:24:24 +0530

    Create Python_003.py

---
+++ Python_003.py
@@ -0,0 +1,14 @@
+# Create a lambda function to print the cube of a number
+
+n = int(input("Enter a number: "))
+
+cube = lambda x : x**3
+
+print(cube(n))
+
+'''
+Output:
+
+Enter a number: 5
+125
+'''
