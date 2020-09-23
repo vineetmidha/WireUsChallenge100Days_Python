c48fff784e0a9535c94fb9b90d473f10c9a164a6
vineetmidha <50925587+vineetmidha@users.noreply.github.com>
2020-09-23 12:09:50 +0530

    Create Python_05.py

---
+++ Python_05.py
@@ -0,0 +1,22 @@
+'''
+5.  WAP to check if a person is eligible to vote or not.
+'''
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not eligible")
+    
+    
+'''
+Output:
+Enter your age: 10
+Not eligible
+Enter your age: 20
+Eligible
+>>> 
+Enter your age: 18
+Eligible
+'''
