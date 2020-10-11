9c2a7a574f15b12067d62d50dbff106fd6fca9d9
vineetmidha <50925587+vineetmidha@users.noreply.github.com>
2020-10-11 20:06:21 +0530

    Create Python_020.py
    20.	WAP to print all the uppercase characters present in the string. 
    Input: TheWireUsChallenge
    Output: ['T','W','U','C']

---
+++ Python_020.py
@@ -0,0 +1,18 @@
+# Problem statement in description
+
+s = input()
+
+uppers = []
+
+for i in s:
+    if i.isupper():
+        uppers.append(i)
+
+print(uppers)
+
+'''
+
+Input: TheWireUsChallenge
+Output: ['T', 'W', 'U', 'C']
+
+'''
