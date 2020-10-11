91714ea52262b082fd8c5ccd10e2d36eece22e01
vineetmidha <50925587+vineetmidha@users.noreply.github.com>
2020-10-11 18:56:03 +0530

    Create Python_017.py
    17.	WAP to print all the multiples of 7 between 1 and 100 and append into a list.

---
+++ Python_017.py
@@ -0,0 +1,12 @@
+# Problem statement in description
+
+nums = [i for i in range(1,101) if i % 7 == 0]
+
+print(nums)
+
+'''
+Output:
+
+[7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98]
+
+'''
