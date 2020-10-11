992cc72a91a9907a89da93de48714fabb6076c35
vineetmidha <50925587+vineetmidha@users.noreply.github.com>
2020-10-11 18:49:33 +0530

    Create Python_012.py
    WAP to print a grimming face, slight smiling face and winking face using CDLR method.

---
+++ Python_012.py
@@ -0,0 +1,13 @@
+# Problem statement in description
+
+print("\U0001f600") 
+
+# grinning face 
+print("\N{grinning face}") 
+  
+# slightly smiling face 
+print("\N{slightly smiling face}") 
+  
+# winking face 
+print("\N{winking face}")
+
