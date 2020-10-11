a226650075de7d0312b17ff2c012e66e6a05d586
vineetmidha <50925587+vineetmidha@users.noreply.github.com>
2020-10-11 19:59:30 +0530

    Create Python_018.py
    18.	WAP to print the factorial value of a given number n.

---
+++ Python_018.py
@@ -0,0 +1,22 @@
+# Problem statement in description
+
+n = int(input())
+
+p = 1
+
+for i in range(1,n+1):
+    p *= i
+
+print("Factorial of ", n, "=", p)n = int(input())
+
+p = 1
+
+for i in range(1,n+1):
+    p *= i
+
+print("Factorial of ", n, "=", p)
+
+'''
+Input: 6
+Output: Factorial of 6 = 720
+'''
