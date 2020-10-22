b2a9038485564b623e6b9af927e2ba27a8d60f0b
vineetmidha <50925587+vineetmidha@users.noreply.github.com>
2020-10-22 16:57:34 +0530

    Create Python_036.py
    36.	WAP to construct the following pattern, using a nested for loop.
    
    *
    **
    ***
    ****
    *****
    ****
    ***
    **
    *

---
+++ Python_036.py
@@ -0,0 +1,26 @@
+# Problem statement in description
+
+n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*", end="")
+    print()
+
+for i in range(4,0,-1):
+    for j in range(1,i+1):
+        print("*", end="")
+    print()
+
+'''
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+'''
